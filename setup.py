bc2491d8631e2836480c57ffe0671692a2b158c3
Tomer Shay <tomershay100@gmail.com>
2022-06-01 22:00:44 +0300

    version 1.1.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if path.isfile(requirementPath):
 
 setup(
     name="mp3stego-lib",
-    version="1.1.0",
+    version="1.1.1",
     author="Aviad Seady, Tomer Shay, Lee Zaid",
     author_email="aviadevelops@gmail.com, tomershay100@gmail.com, lizizaid@gmail.com",
     description="mp3 steganography library",
